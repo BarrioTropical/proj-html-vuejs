22dd7c663f248aa502f1eb7bc554422bf7ca4cbe
BarrioTropical <barriotropical@gmail.com>
2022-11-21 15:59:54 +0100

    footer structure
    Signed-off-by: BarrioTropical <“barriotropical@gmail.com”>

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,7 +1,19 @@
 <template>
     <!-- top footer section -->
+    <div class="row">
+        <div class="col-sm-6 col-lg-3">Logo and description</div>
+        <div class="col-sm-6 col-lg-3">Menù shop by brand</div>
+        <div class="col-sm-6 col-lg-3">Useful links</div>
+        <div class="col-sm-6 col-lg-3">Newsletter section</div>
+    </div>
     <!-- sub footer section -->
+    <div class=" text-center">
+        Menu section and social
+    </div>
     <!-- copyright section -->
+    <div>
+    Copyright section
+    </div>
 </template>
 
 <script>
@@ -11,5 +23,7 @@
 </script>
 
 <style lang="scss" scoped>
+@import "../assets/styles/generals.scss";
+
 
 </style>
