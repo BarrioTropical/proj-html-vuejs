a6989b7afd1d18a642946eb604f59063f745c00e
BarrioTropical <barriotropical@gmail.com>
2022-11-22 15:14:42 +0100

    main section
    Signed-off-by: BarrioTropical <“barriotropical@gmail.com”>

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -43,16 +43,129 @@
         </div>
     </div>
 
-
-
     <!-- banner section -->
+
+    <div class="p-5 text-center bg-image banner-new">
+    <div>
+      <div class="d-flex justify-content-center align-items-center h-100">
+        <div class="text-white">
+          <h5 class="mb-3">Find the best animal supplies</h5>
+          <h2 class="mb-3">New arrivals weekly</h2>
+          <a class="btn mybutton btn-lg" href="#" role="button">Learn more about us</a>
+        </div>
+      </div>
+    </div>
+  </div>
     <!-- best sellers section -->
+    <div class="row">
+      <div class="col-6">
+      <h3>All-time best sellers</h3>
+      <h2>Items everyone loves</h2>
+      </div>
+      <div class="col-6"><a class="btn mybutton btn-lg" href="#" role="button">View all products</a></div>
+    </div>
+
+
+    <div class="row justify-content-between text-center container">
+        <div class="col-sm-6 col-lg-3"><img src="/public/img/product-21.jpg" alt=""><p>Transport cage</p></div>
+        <div class="col-sm-6 col-lg-3"><img src="/public/img/product-20.jpg" alt=""><p>Dog leash</p></div>
+        <div class="col-sm-6 col-lg-3"><img src="/public/img/product-16.jpg" alt=""><p>Animal transport cage</p></div>
+        <div class="col-sm-6 col-lg-3"><img src="/public/img/product-11.jpg" alt=""><p>Colorful cat leash</p></div>
+    </div>
     <!-- testimonial section -->
+    <div class="mybg-green">
+    <div class="container text-center my-padding text-center">
+    <h2>Users testimonials</h2>
+    <div class="row justify-content-between text-center container">
+        <div class="col-sm-6 col-lg-4"><img src="/public/img/avatar-3.jpg" alt="">
+        <p>Auctor est habitasse amet nunc, interdum vel mattis sodales cras. Ut nulla quis nunc, tincidunt eu. Eu.</p>
+        <p>Lisa Smith, ThemeFusion</p>
+        </div>
+        <div class="col-sm-6 col-lg-4"><img src="/public/img/avatar-1.jpg" alt="">
+        <p>Donec sollicitudin molestie malesuada. Vivamus suscipit tortor eget felis porttitor volutpat vestibulum ac diam sit.</p>
+        <p>Melissa Green, ThemeFusion</p>
+        </div>
+        <div class="col-sm-6 col-lg-4"><img src="/public/img/avatar-4.jpg" alt="">
+        <p>Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Proin eget tortor risus vestibulum ante ipsum.</p>
+        <p>Sam Lewis, ThemeFusion</p>
+        </div>
+    </div>
+    </div>
+    </div>
+
     <!-- newsletter section -->
+    <div class="jumbotron mybg text-white container mypadding-r">
+    <div class="row">
+    <div class="col">
+      <h2 class="pb-4 text-green">Join our newsletter</h2>
+      <h5 class="pb-4 text-green">Volutpat vel turpis nulla lorem sed semper. Aliquam sagittis sem libero viverra vehicula nullam ut nisl.</h5>
+      <div class="subscription-wrapper">
+          <div class="d-flex subscription-content justify-content-between align-items-center flex-column flex-md-row text-center text-md-left">
+            <form action="#" class="row flex-fill">
+              <div class="col-lg-7 my-md-2 my-2">
+                <input type="email" class="form-control px-4 border-0 w-100 text-center text-md-left" id="email" placeholder="Insert your email ...*" name="email">
+              </div>
+              <div class="col-lg-5 my-md-2 my-2">
+                <button type="submit" class="btn btn-primary btn-lg border-0 w-100">Subscribe</button>
+              </div>
+            </form>
+          </div>
+        </div>    </div>
+    <div class="col"></div>
+    </div>
+    </div>
     <!-- blog section -->
+    <div class="text-center">
+    <div class="container text-center my-padding">
+    <h2>Get the best tips & tricks</h2>
+    <p>Recent articles</p>
+    </div>
+    <div class="row justify-content-between text-center container">
+        <div class="col-sm-6 col-lg-3"><img src="/public/img/blog-1.jpg" alt=""><p>How to ‘loose leash walk’ your dog</p></div>
+        <div class="col-sm-6 col-lg-3"><img src="/public/img/blog-2.jpg" alt=""><p>Stop cats from scratching furniture</p></div>
+        <div class="col-sm-6 col-lg-3"><img src="/public/img/blog-3.jpg" alt=""><p>Where to buy the best pet food</p></div>
+        <div class="col-sm-6 col-lg-3"><img src="/public/img/blog-4.jpg" alt=""><p>Proper care for your pet toys</p></div>
+    </div>
+    <div><a class="btn btn-lg buttonFood" href="#" role="button">Read all articles</a></div>
+    </div>
     <!-- find section -->
+    <div class="row">
+    <div class="col find-1">
+      <h5 class="pb-4">Find the best animal supplies</h5>
+      <h2 class="pb-4">Popular accessories</h2>
+      <p><a class="btn mybutton btn-lg" href="#" role="button">View all toys accesories</a></p>
+    </div>
+    <div class="col find-1">
+      <h5 class="pb-4">Find the best animal supplies</h5>
+      <h2 class="pb-4">Popular accessories</h2>
+      <p><a class="btn mybutton btn-lg" href="#" role="button">View all toys accesories</a></p>
+    </div>
+    </div>
     <!-- value section -->
+
+    <div class="mybg-green pt-4 text-center">
+        <div class="row">
+        <div class="col-6 col-md-3"><i class="fa-regular fa-truck"></i><p>Free worldwide deliveries</p></div>
+        <div class="col-6 col-md-3"><i class="fa-regular fa-map"></i><p>Find stores near you</p></div>
+        <div class="col-6 col-md-3"><i class="fa-regular fa-dollar-sign"></i><p>Best prices guarranteed</p></div>
+        <div class="col-6 col-md-3"><i class="fa-regular fa-credit-card"></i><p>All credit card accepted</p></div>
+        </div>
+    </div>
     <!-- new arrival section -->
+    <div class="text-center">
+    <div class="container text-center my-padding">
+    <h2>New products arrival</h2>
+    <p>Latest products</p>
+    </div>
+    <div class="row justify-content-between text-center container">
+        <div class="col-sm-6 col-lg-4"><img src="/public/img/product-8.jpg" alt=""><p>Bed (1)</p></div>
+        <div class="col-sm-6 col-lg-4"><img src="/public/img/product-2.jpg" alt=""><p>Food (6)</p></div>
+        <div class="col-sm-6 col-lg-4"><img src="/public/img/product-3.jpg" alt=""><p>Toys (6)</p></div>
+        <div class="col-sm-6 col-lg-4"><img src="/public/img/product-4.jpg" alt=""><p>Transport (6)</p></div>
+        <div class="col-sm-6 col-lg-4"><img src="/public/img/product-5.jpg" alt=""><p>Transport (6)</p></div>
+        <div class="col-sm-6 col-lg-4"><img src="/public/img/product-10.jpg" alt=""><p>Transport (6)</p></div>
+    </div>
+    </div>
 </template>
 
 <script>
@@ -69,9 +182,46 @@ import JumboComponent from './JumboComponent.vue';
 @import '../assets/styles/variables';
 @import '../assets/styles/generals.scss';
 
+.find-1{
+  background-image: url(/public/img/banner-8-2x.jpg);
+  height: 400px;
+  background-size: cover;
+  vertical-align: middle;
+  text-align: center;
+  color: white;
+  margin: 0 auto;
+}
+
+
 .my-padding{
     padding-top: 100px;
     padding-bottom: 50px;
+}
+
+.banner-new{
+  background-image: url(/public/img/banner-3-2x-scaled.jpg);
+  height: 400px;
+  background-size: cover;
+  vertical-align: middle;
+}
+.mybg{
+    background: url(/public/img/banner-7-2x-scaled.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 60vh;
+    padding: 60px;
+}
+
+.text-green{
+  color: $darkgreen;
+}
+
+.mypadding-r{
+  margin-top: -30px;
+}
+
+.mask{
+background-color: rgba(0, 0, 0, 0.6);
 }
 
 h4{
@@ -86,6 +236,11 @@ h4{
     background-color: $lightgreen;
     color:$white;
     border-radius: 30px;
+}
+
+.mybutton{
+  background-color: $white;
+  border-radius: 30px;
 }
 
 </style>
