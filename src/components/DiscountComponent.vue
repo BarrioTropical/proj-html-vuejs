ee58a3c96562c1fcca9363a935933ff0ff8ddc82
BarrioTropical <barriotropical@gmail.com>
2022-11-22 11:06:48 +0100

    product category
    Signed-off-by: BarrioTropical <“barriotropical@gmail.com”>

---
+++ src/components/DiscountComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="mybg-green p-4 text-center">
+        Get $25 OFF your first purchase of our homemade pet food! Visit the shop
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'DiscountComponent',
+    }
+</script>
+
+<style lang="scss" scoped>
+.mybg-green{
+    background-color: #1E3120;
+}
+</style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,7 +1,18 @@
 <template>
     <!-- jumbo section -->
     <JumboComponent/>
+    <DiscountComponent/>
     <!-- product's category section -->
+    <div class="container p-4 text-center">
+    <h3>Browse by category</h3>
+    <p>Augue purus et, tincidunt condimentum mauris. At nibh rutrum mi in. Nisi, vitae interdum eleifend dui, consequat nulla rhoncus dictum. Viverra.</p>
+    </div>
+    <div class="row justify-content-between text-center">
+        <div class="col-sm-6 col-lg-3"><img src="/public/img/product-9.jpg" alt=""><p>Bed (1)</p></div>
+        <div class="col-sm-6 col-lg-3"><img src="/public/img/product-3.jpg" alt=""><p>Food (6)</p></div>
+        <div class="col-sm-6 col-lg-3"><img src="/public/img/product-2.jpg" alt=""><p>Toys (6)</p></div>
+        <div class="col-sm-6 col-lg-3"><img src="/public/img/product-4.jpg" alt=""><p>Transport (6)</p></div>
+    </div>
     <!-- food section -->
     <!-- banner section -->
     <!-- best sellers section -->
@@ -14,14 +25,17 @@
 </template>
 
 <script>
+import DiscountComponent from './DiscountComponent.vue';
 import JumboComponent from './JumboComponent.vue';
 
     export default {
     name: "MainComponent",
-    components: { JumboComponent }
+    components: { JumboComponent, DiscountComponent }
 }
 </script>
 
 <style lang="scss" scoped>
-
+img{
+    max-width: 300px;
+}
 </style>
