466775c6e40b03cb12ea3640472fe5202318fbcb
BarrioTropical <barriotropical@gmail.com>
2022-11-22 22:15:39 +0100

    icon value
    Signed-off-by: BarrioTropical <“barriotropical@gmail.com”>

--- src/components/Main Components/ValueComponent.vue
+++ src/components/Main Components/ValueComponent.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="bg-discount pt-4 text-center">
-        <div class="row">
-        <div class="col-6 col-md-3"><i class="fa-regular fa-truck"></i><p>Free worldwide deliveries</p></div>
-        <div class="col-6 col-md-3"><i class="fa-regular fa-map"></i><p>Find stores near you</p></div>
-        <div class="col-6 col-md-3"><i class="fa-regular fa-dollar-sign"></i><p>Best prices guarranteed</p></div>
-        <div class="col-6 col-md-3"><i class="fa-regular fa-credit-card"></i><p>All credit card accepted</p></div>
+        <div class="row p-3">
+        <div class="col-6 col-md-3"><i class="fa-regular fa-truck fa-4x"></i><p>Free worldwide deliveries</p></div>
+        <div class="col-6 col-md-3"><i class="fa-regular fa-map fa-4x"></i><p>Find stores near you</p></div>
+        <div class="col-6 col-md-3"><i class="fa-regular fa-dollar-sign fa-4x"></i><p>Best prices guarranteed</p></div>
+        <div class="col-6 col-md-3"><i class="fa-regular fa-credit-card fa-4x"></i><p>All credit card accepted</p></div>
         </div>
     </div>
 </template>
